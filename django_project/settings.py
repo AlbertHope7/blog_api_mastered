1eb3212cc038808d3c8759643521fcbfd144e389
AlbertHope7 <byiringiroalbert20@gmail.com>
2023-03-03 16:24:46 +0200

    add custom user model

--- django_project/settings.py
+++ django_project/settings.py
@@ -123,3 +123,6 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# update the auth_user_model
+AUTH_USER_MODEL = "accounts.CustomUser"
