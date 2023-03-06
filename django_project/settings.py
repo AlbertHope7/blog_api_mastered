6e8b41f62d1d25a0843d28ba80a5e81f8094cb79
AlbertHope7 <byiringiroalbert20@gmail.com>
2023-03-06 10:18:57 +0200

    add settings to allow specific cross-domain requests

--- django_project/settings.py
+++ django_project/settings.py
@@ -42,11 +42,13 @@ INSTALLED_APPS = [
     "posts.apps.PostsConfig",
     # 3rd party
     "rest_framework",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -136,3 +138,12 @@ REST_FRAMEWORK = {
         "rest_framework.permissions.AllowAny",
     ]
 }
+
+# whitelist external domains
+CORS_ORIGIN_WHITELIST = (
+    "http://localhost:3000",
+    "http://localhost:8000",
+)
+
+# allow cross-domain requests from frontend
+CSRF_TRUSTED_ORIGINS = ["http://localhost:3000"]
