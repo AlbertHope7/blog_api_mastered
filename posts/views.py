664284435df15cd87d53f6fa5327093cc9b6560e
AlbertHope7 <byiringiroalbert20@gmail.com>
2023-03-07 08:55:29 +0200

    add viewsets and routers

--- posts/views.py
+++ posts/views.py
@@ -14,6 +14,6 @@ class PostViewSet(viewsets.ModelViewSet):
 
 
 class UserViewSet(viewsets.ModelViewSet):
-    permission_classes = [IsAdminUser]
+    permission_classes = (IsAdminUser,)
     queryset = get_user_model().objects.all()
     serializer_class = UserSerializer
